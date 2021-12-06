79c946b38c4aa2559fe0c5c6404c5205ac542e26
AkhilchandranR <akhilchandran771@gmail.com>
2021-12-06 17:43:06 +0530

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import { IconButton } from '@mui/material';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+            <img className="header__logo" 
+            src="https://1000logos.net/wp-content/uploads/2018/07/tinder-emblem-500x281.jpg"
+            alt="tinder-logo"
+            />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+        </div>
+    )
+}
+
+export default Header
